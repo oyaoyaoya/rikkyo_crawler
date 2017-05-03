9654dc2fc7c588153530da8fd2c90389fc2dc966
SugiKent <ynmk1995@gmail.com>
2017-05-03 10:13:58 +0900

    refs #4 年度を追加

--- nokogiri_crawler.rb
+++ nokogiri_crawler.rb
@@ -45,6 +45,9 @@ class NokogiriCrawler
 		end
 		# 各授業ページへのリンクを取得
 		lesson_row_formated << "https://sy.rikkyo.ac.jp" + lesson_row.css("td:nth-child(3) a")[0][:href]
+		# 年度を追加
+		# プログラム上は現在の年を追加
+		lesson_row_formated << Time.now.year
 
 		lesson_row_formated.flatten!.map do |l|
 			# 空文字ならnilを
